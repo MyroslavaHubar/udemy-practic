b9f1725b1d059cd548985a59cb2e110d6fa22bb5
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-05-08 00:03:57 +0200

    addlesson -1

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -62,14 +62,153 @@
 
 /*==================================
 ======================================*/
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
 
-if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
-  console.log("Done!");
-}
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+//   console.log("Done!");
+// }
 
 /*==================================
 ======================================*/
+// console.log(NaN || 2 || undefined);
+
+// console.log(NaN && 2 && undefined);
+
+// console.log(1 && 2 && 3);
+
+// console.log((!1 && 2) || !3);
+
+// console.log(25 || (null && !3));
+
+// console.log(NaN || null || !3 || undefined || 5);
+
+// console.log(NaN || (null && !3 && undefined) || 5);
+
+// console.log((5 === 5 && 3 > 1) || 5);
+
+/*======================================================*/
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger === 3 && cola) || (fries === 3 && nuggets)) {
+//   console.log("Done!");
+// }
+
+/*=====================================================*/
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//   console.log("Done!");
+// }
+
+/*================================================*/
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+// if ((hamburger && cola) || (fries === 3 && nuggets)) {
+//   console.log("Done!");
+
+/*===================================================*/
+
+// При помощи цикла for выведите чётные числа от 2 до 10.
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+/*====================================================*/
+// Напишите цикл, который предлагает prompt ввести число,
+// большее 100. Если посетитель ввёл другое число – попросить
+// ввести ещё раз, и так далее.
+
+// Цикл должен спрашивать число пока либо посетитель
+// не введёт число, большее 100, либо не нажмёт кнопку Отмена(ESC).
+
+// Предполагается, что посетитель вводит только числа.
+// Предусматривать обработку нечисловых строк в этой задаче необязательно.
+
+// let num;
+
+// do {
+//   num = prompt("Введите число больше 100?", 0);
+// } while (num <= 100 && num);
+
+// console.log(num);
+
+/*===========================================================*/
+// Напишите код, который выводит все простые числа
+// из интервала от 2 до n.
+
+// Для n = 10 результат должен быть 2,3,5,7.
+
+// let n = 10;
+
+// nextPrime: for (let i = 2; i <= n; i++) {
+//   // Для всех i...
+
+//   for (let j = 2; j < i; j++) {
+//     // проверить, делится ли число..
+//     if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+//   }
+
+//   console.log(i); // простое число
+// }
+
+/*====*/
+// function isPrime(num) {
+//   if (num <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function printPrimesInRange(n) {
+//   for (let i = 2; i <= n; i++) {
+//     if (isPrime(i)) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// const n = 20; // Можете изменить n на любое другое число
+// printPrimesInRange(n);
+
+/*=======================================*/
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = "";
+// const length = 7;
+
+// for (let i = 0; i < length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+//   }
+
+//   result += "\n";
+// }
+
+// console.log(result);
+
+/*====================================================*/
