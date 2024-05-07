68aa9acb276cd14294b19608dc26d0ae8a9defcc
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-05-08 01:37:55 +0200

    update lesson 1 file

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -293,3 +293,105 @@
 // }
 
 // fifthTask();
+/*=============================================*/
+// Задача на формирование фигуры
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+// function drawPyramid(height) {
+//   for (let i = 1; i <= height; i++) {
+//     let spaces = ' '.repeat(height - i);
+//     let stars = '*'.repeat(2 * i - 1);
+//     console.log(spaces + stars);
+//   }
+// }
+
+// const height = 6;
+// drawPyramid(height);
+
+/*============================================*/
+// Заполните новый массив (result) числами из старого (arr).
+// Количество элементов в массиве можно получить как arr.length,
+// а к элементам обращаемся все так же: arr[0], arr[1] и тд.
+// Должен получиться точно такой же массив
+
+// function firstTask() {
+//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//   const arr = [3, 5, 8, 16, 20, 23, 50];
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+//   }
+//   console.log(result);
+//   return result;
+// }
+
+// firstTask();
+
+/*===========================================*/
+// Измените данный массив так, чтобы все числа были увеличены в 2 раза,
+//  а если попадается строка - то к ней было добавлено
+// " - done".
+// Для определения типа данных используйте typeof();
+// Должно получиться: [ 10, 20, 'Shopping - done', 40,
+// 'Homework - done' ]
+
+// function secondTask() {
+//   // Значения массива менять нельзя, тут он проверяется
+//   //    автоматически именно на эти значения
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//   for (let i = 0; i < data.length; i++) {
+//     if (typeof data[i] === 'number') {
+//       data[i] = data[i] * 2;
+//     } else if (typeof data[i] === 'string') {
+//       data[i] = `${data[i]} - done`;
+//     }
+//   }
+//   console.log(data);
+//   return data;
+// }
+
+// secondTask();
+/*===========*/
+// function secondTask() {
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//   const newData = data.map(item => {
+//     if (typeof item === 'number') {
+//       return item * 2;
+//     } else if (typeof item === 'string') {
+//       return item + ' - done';
+//     } else {
+//       return item;
+//     }
+//   });
+
+//   return newData;
+// }
+
+// console.log(secondTask());
+
+/*========================================================*/
+// Разверните массив data наоборот при помощи цикла и
+//  запишите данные в массив result.
+// Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+
+// function thirdTask() {
+//   // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+//   const result = [];
+//   for (let i = data.length - 1; i >= 0; i--) {
+//     result.push(data[i]);
+//   }
+//   console.log(result);
+//   return result;
+// }
+
+// thirdTask();
+
+/*=====================================*/
