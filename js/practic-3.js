0e913f405d2aac20add9ca5cb38fe7553fcc393c
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-05-25 02:24:01 +0200

    update task 3 file

--- js/practic-3.js
+++ js/practic-3.js
@@ -66,4 +66,29 @@ function detectPersonalLevel() {
 
 detectPersonalLevel();
 
-console.log(personalMovieDB);
+// function showMyDB() {
+//   if (personalMovieDB.privat === false) {
+//     console.log(personalMovieDB);
+//   }
+// }
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = genre;
+  }
+}
+
+// function writeYourGenres() {
+//   for (let i = 1; i <= 3; i++) {
+//     personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+//   }
+// }
+writeYourGenres();
